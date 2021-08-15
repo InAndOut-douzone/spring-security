dbf4ee0daef036f22657d1e6bb220a7f9dce5589
kimjeonghyun <rlawjdgus051@naver.com>
2021-08-15 11:38:25 +0900

    security #1

--- src/main/java/com/douzone/securityEx/SecurityExApplication.java
+++ src/main/java/com/douzone/securityEx/SecurityExApplication.java
@@ -3,22 +3,6 @@ package com.douzone.securityEx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Bootstrap Class
- * 
- * 1. 스프링 부트 애플리케이션의 부트스트래핑(Bootstrapping)
- * 2. 설정 클래스로 역할 (Configuration Class)
- * 
- */
-
-// @SpringBootApplication 메타 어노테이션
-// @SpringBootConfiguration(alias for @Configuration)
-// @ComponentScan
-// @AutoConfiguration 
-// 
-//
-//
-
 @SpringBootApplication
 public class SecurityExApplication {
 
--- src/main/java/com/douzone/securityEx/controller/MainController.java
+++ src/main/java/com/douzone/securityEx/controller/MainController.java
@@ -7,12 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
 public class MainController {
-
-	@GetMapping("/list")
+	
+	@GetMapping("/")
 	public ResponseEntity<?> main(){
-		return new ResponseEntity<>("1", HttpStatus.OK);	// 응답(Response)에 해당하는 HttpHeader와 HttpBody를 포함하는 클래스
+		return new ResponseEntity<>("main", HttpStatus.OK);	// 응답(Response)에 해당하는 HttpHeader와 HttpBody를 포함하는 클래스
 	}
 
+	@GetMapping("/api/user")
+	public ResponseEntity<?> user(){
+		return new ResponseEntity<>("user", HttpStatus.OK);	// 응답(Response)에 해당하는 HttpHeader와 HttpBody를 포함하는 클래스
+	}
+	
+	@GetMapping("/api/admin")
+	public ResponseEntity<?> admin(){
+		return new ResponseEntity<>("admin", HttpStatus.OK);	// 응답(Response)에 해당하는 HttpHeader와 HttpBody를 포함하는 클래스
+	}
+	
+	@GetMapping("/api/manage")
+	public ResponseEntity<?> manager(){
+		return new ResponseEntity<>("manager", HttpStatus.OK);	// 응답(Response)에 해당하는 HttpHeader와 HttpBody를 포함하는 클래스
+	}
 }
